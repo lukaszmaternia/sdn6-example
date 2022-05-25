41a6c6c4f58308074a25d6594fb2f723cbd1666f
Lukasz Maternia <lukasz.maternia@qiagen.com>
2022-05-25 08:42:45 +0200

    Cleanup

--- src/main/java/org/neo4j/tips/sdn/sdn6/movies/ParentToChild.java
+++ src/main/java/org/neo4j/tips/sdn/sdn6/movies/ParentToChild.java
@@ -1,13 +1,7 @@
 package org.neo4j.tips.sdn.sdn6.movies;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-import org.springframework.data.neo4j.core.schema.GeneratedValue;
-import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
-import org.springframework.data.neo4j.core.schema.TargetNode;
 
 @RelationshipProperties
 public class ParentToChild extends Relationship<Person> {
