41f1787fb6f799bfbbb7bdc8bcd5b803c2a40157
Lukasz Maternia <lukasz.maternia@qiagen.com>
2022-05-25 08:41:52 +0200

    Test

--- src/main/java/org/neo4j/tips/sdn/sdn6/movies/PeopleRepository.java
+++ src/main/java/org/neo4j/tips/sdn/sdn6/movies/PeopleRepository.java
@@ -11,13 +11,16 @@ import org.springframework.data.neo4j.repository.query.Query;
  */
 public interface PeopleRepository extends Neo4jRepository<Person, UUID> {
 
-	@Query(
-			"MATCH (person:Person {name: $name}) " +
-					"OPTIONAL MATCH (person)<-[parentRel:PARENT_TO_CHILD]-(parent:Person) " +
-					"RETURN person.id as id, " +
-					"person, "+
-					"collect(DISTINCT parent) AS parents, "+
-					"collect(DISTINCT parentRel) AS parentRels"
-	)
-	Optional<PersonWrapper> getPersonWithParents(String name);
+    @Query(
+            "MATCH (person:Person {name: $name}) " +
+                    "OPTIONAL MATCH (person)<-[parentRel:PARENT_TO_CHILD]-(parent:Person) " +
+                    "OPTIONAL MATCH (person)-[childRel:PARENT_TO_CHILD]->(child:Person) " +
+                    "RETURN person.id as id, " +
+                    "person, " +
+                    "collect(DISTINCT parent) AS parents, " +
+                    "collect(DISTINCT parentRel) AS parentRels, " +
+                    "collect(DISTINCT child) AS children, " +
+                    "collect(DISTINCT childRel) AS childRels"
+    )
+    Optional<PersonWrapper> getPersonWithParents(String name);
 }
